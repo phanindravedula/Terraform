7f8cc0603bda24098d89fbf58a6718a3f2ed5fa8
Vedula Phanindra <phanindravedula@gmail.com>
2022-09-01 11:02:02 +0530

    Add files via upload

--- StorageAccount/provider.tf
+++ StorageAccount/provider.tf
@@ -1,6 +1,7 @@
 
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
+
 terraform {
   required_providers {
     azurerm = {
@@ -9,10 +10,20 @@ terraform {
     }
   }
 
-
+  backend "azurerm" {
+        resource_group_name  = "Phanindra_DevOps_Demo"
+        storage_account_name = "phanindrasa01"
+        container_name       = "terraform"
+        key                  = "storageaccountterraform.tfstate"
+        sas_token = "sp=racwl&st=2022-09-01T03:57:04Z&se=2022-09-01T11:57:04Z&spr=https&sv=2021-06-08&sr=c&sig=JROdLg9wwOsnJATwpPOxmLjZUM3eDKdsc7ezRCi2x0w%3D"
+    }
 }
 
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+
+  subscription_id = "5ee40778-4886-47ce-9ec0-4d6048bb84e1"
+  client_id       = "2c18f6da-4db4-485d-98ba-b0c64e6fdf74"
+  client_secret   = "kMo8Q~Pkq~0FnFbBYK5Ij3vy4urWCwrtDLIQ1b_V"
+  tenant_id       = "c7738379-585d-496e-af73-fbb7b4ab5136"
 }
